a572241e17029bf507b7e091a82f6f4d6c73992f
Nick Foxen <foxen.nicholas@gmail.com>
2021-12-10 12:38:53 -0800

    Still working on the banner

--- src/Banner.js
+++ src/Banner.js
@@ -17,6 +17,10 @@ function Banner() {
     }
     fetchData();
   }, []);
+
+  function truncate(str, n) {
+    return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+  }
   
   return (
     <header className="banner"
@@ -27,7 +31,6 @@ function Banner() {
       }}
     >
       <div className="banner_contents">
-        {/* Title */}
         <h1 className="banner_title">
           {movie?.title || movie?.name || movie?.original_name}
         </h1>
@@ -35,10 +38,7 @@ function Banner() {
           <button className="banner_button">Play</button
           ><button className="banner_button">My List</button>
         </div>
-        <h1 className="banner_description">
-         {movie?.overview} 
-        </h1>
-        {/* Description */}
+        <h1 className="banner_description">{truncate(movie?.overview,150)}</h1>
       </div>
     </header>
   )

2696529c3cbdd3ecba46b61089ee08a6063143ba
Nick Foxen <foxen.nicholas@gmail.com>
2021-12-26 17:13:36 -0800

    Finished navbar and now adding pop up trailers

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,9 +1,24 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import "./Navbar.css";
 
 function Navbar() {
+  const [show, setHandleShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        setHandleShow(true);
+      } else setHandleShow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    }
+  }, []);
+
   return (
-    <div className="navbar">
-      
+    <div className={`navbar ${show && "nav_black"}`}>
+      <img className="nav_logo" src="./netflix-logo.png" alt="Netflix logo" />
+      <img className="nav_avatar" src="https://occ-0-1339-1340.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABeUqbfriC_pGWtwTa1KOx-ZSiQYa7ltLkOuduGxY_GRRc41ugYJNGYHe4LNcmshST4qkRSENvcs2xFomPc0rtX8wq2NG.png?r=b97" alt="Profile avatar" />
     </div>
   )
 }
--- src/Row.js
+++ src/Row.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from 'react'
-import  axios from './axios';
+import axios from './axios';
+import YouTube from "react-youtube";
 import './Row.css';
 
 const baseURL = "https://image.tmdb.org/t/p/original/";
 
 function Row({ title, fetchURL, isLargeRow }) {
   const [movies, setMovies] = useState([]);
-
+  const [trailerUrl, setTrailerUrl] = useState("");
   // Code which runs based on a specific condition/variable
   useEffect(() => {
     // if [], run once when the row loads, and don't run again
@@ -17,6 +18,21 @@ function Row({ title, fetchURL, isLargeRow }) {
     };
     fetchData();
   }, [fetchURL]);
+
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+
+      autoplay: 1,
+    },
+  };
+
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    }
+  }
 
   return (
     <div className="movie_row">
@@ -29,6 +45,7 @@ function Row({ title, fetchURL, isLargeRow }) {
           src={`${baseURL}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} alt={movie.name} />
         ))}
       </div>
+      {trailerUrl && <YouTube videoId={trailerUrl}  opts={opts} />}
     </div> 
   )
 }
